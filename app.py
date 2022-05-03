922493da6951c444a3368c2c49a3383ac72730f5
Patrick Kozlowski <pjkozlowskijr@gmail.com>
2022-05-03 00:09:22 -0400

    Add formatting to lookup.html

--- app.py
+++ app.py
@@ -10,21 +10,24 @@ def index():
 @app.route("/lookup", methods = ['GET', 'POST'])
 def lookup():
     if request.method == "POST":
-        name = request.form.get("name")
-        url = f"https://pokeapi.co/api/v2/pokemon/{name}"
-        response = requests.get(url).json()
+        pokemon = request.form.get("name")
+        url = f"https://pokeapi.co/api/v2/pokemon/{pokemon}"
+        response = requests.get(url)
         if not response.ok:
             error_string = "There was an error. Please try again."
             return render_template("lookup.html.j2", error=error_string)
         if not response.json():
             error_string = "There was an error, most likely because the Pokemon you entered does not exist. Please try again."
             return render_template("lookup.html.j2", error=error_string)
+        poke_data = response.json()
         pokemon_dict = {
-            "name": response["species"]["name"],
-            "sprite": response["sprites"]["front_shiny"],
-            "base_experience": response["base_experience"],
-            "ability_name": response["abilities"][0]["ability"]["name"],
-            "attack_base": response["stats"][1]["base_stat"],
-            "hp_base": response["stats"][0]["base_stat"],
-            "defense_base":response["stats"][2]["base_stat"],
+            "name": poke_data["species"]["name"],
+            "sprite": poke_data["sprites"]["front_shiny"],
+            "base_experience": poke_data["base_experience"],
+            "ability_name": poke_data["abilities"][0]["ability"]["name"],
+            "attack_base": poke_data["stats"][1]["base_stat"],
+            "hp_base": poke_data["stats"][0]["base_stat"],
+            "defense_base":poke_data["stats"][2]["base_stat"],
         }
+        return render_template("lookup.html.j2", pokemon_table = pokemon_dict)
+    return render_template("lookup.html.j2")
