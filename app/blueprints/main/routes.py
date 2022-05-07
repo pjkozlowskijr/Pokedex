654adfc1d0cda5d097cbaaccf0a90043e4ca6b5c
Patrick Kozlowski <pjkozlowskijr@gmail.com>
2022-05-07 10:08:40 -0400

    Add ID and height to pokemon dict.

--- app/blueprints/main/routes.py
+++ app/blueprints/main/routes.py
@@ -14,17 +14,20 @@ def lookup():
     form = PokeLookupForm()
     if request.method == "POST":
         pokemon = form.poke_name.data.lower()
-        url = f"https://pokeapi.co/api/v2/pokemon/{pokemon}"
-        response = requests.get(url)
-        if not response.ok:
+        url_base = f"https://pokeapi.co/api/v2/pokemon/{pokemon}"
+        response_base = requests.get(url_base)
+        if not response_base.ok:
             flash("There was an error, most likely because the Pokemon you entered does not exist. Please try again.", "danger")
             return render_template("lookup.html.j2", form=form)
-        if not response.json():
+        if not response_base.json():
             flash("There was an unexpected error. Please try again.", "danger")
             return render_template("lookup.html.j2", form=form)
-        poke_data = response.json()
+        poke_data = response_base.json()
+        inches = round(poke_data["height"] * 3.937)
         pokemon_dict = {
             "name": poke_data["species"]["name"],
+            "id": poke_data["id"],
+            "height": f"{str(int(inches//12))}' {str(int(inches%12))}\"",
             "sprite": poke_data["sprites"]["other"]["home"]["front_default"],
             "base_experience": poke_data["base_experience"],
             "ability_name": poke_data["abilities"][0]["ability"]["name"],
